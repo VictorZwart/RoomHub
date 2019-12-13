0b2449136c90e36e1a7e874f16c48f3416e4564d
Robin van der Noord <robinvandernoord@gmail.com>
2019-12-13 18:14:58 +0100

    no more nextras/orm

--- db.php
+++ db.php
@@ -2,29 +2,3 @@
 /* connect here */
 
 /* define db models here */
-
-use Nextras\Orm\Entity\Entity;
-use Nextras\Orm\Relationships\ManyHasOne;
-use Nextras\Orm\Relationships\OneHasMany;
-use Nextras\Orm\Relationships\OneHasOne;
-
-
-
-class Listing extends Entity {
-}
-
-class User extends Entity {
-}
-
-
-/**
- * @property int $room_id    {primary}
- * @property string $description
- * @property float $price
- * @property string $size
- * @property string $type
- * @property
- * @property User $owner_id {m:1 User}
- */
-class Room extends Entity {
-}
