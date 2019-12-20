accdc5753aa3e2c84ec56e9b034328651a0b0a87
Erik Storm <erikbstorm@outlook.com>
2019-12-20 13:03:18 +0100

    added css

---
+++ tpl/footer.php
@@ -0,0 +1,5 @@
+        <footer>
+            <h1>Dit is de footer</h1>
+        </footer>
+    </body>
+</html>
---
+++ tpl/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="NL">
+    <head>
+        <!-- General information -->
+        <title>Roomhub</title>
+
+        <!-- CSS and styling -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/main.css" type="text/css">
+        <link rel="stylesheet" href="css/general.css" type="text/css">
+
+        <!-- Javascript -->
+        <script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <!-- Meta information -->
+        <meta name="description" content="A website for displaying and sharing rooms in Groningen, Netherlands.">
+
+    </head>
+    <body>
+        <header>
+            <h1> Dit is de header!</h1>
+        </header>
