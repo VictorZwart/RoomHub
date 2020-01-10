8ca394bee85d4004336e21fd0305f52758906515
Robin van der Noord <robinvandernoord@gmail.com>
2020-01-10 11:40:55 +0100

    stop a crash and WIP

--- models/db.php
+++ models/db.php
@@ -11,6 +11,8 @@ use PDOException;
 
 
 // models
+
+// table for listing that allows linking a room
 class ListingTable extends Table {
 	public function initialize(array $config) {
 		// 1 room can have many listings
@@ -18,12 +20,13 @@ class ListingTable extends Table {
 	}
 }
 
+// table for room that allows linking listing
 class RoomTable extends Table {
 	public function initialize(array $config) {
 		// 1 room can have many listings
 		$this->hasMany('Listing')
-		     // ->setConditions(['status' => 'active'])
-		     ->setForeignKey('room_id');
+			// ->setConditions(['status' => 'active'])
+			 ->setForeignKey('room_id');
 	}
 }
 
@@ -55,7 +58,7 @@ class DB {
 			ConnectionManager::setConfig('default', ['url' => $dsn]);
 
 			$this->conn = ConnectionManager::get('default');
-		} catch (Exception $e) {
+		} catch(Exception $e) {
 			echo 'something went wrong connecting to the database.';
 			die();
 		}
@@ -75,9 +78,11 @@ class DB {
 	 */
 	function __get($name) {
 		$ucname = 'Roomhub\\' . ucfirst($name) . 'Table';
+
+
 		if (class_exists($ucname)) {
 			// custom table class exists, use that
-			return TableRegistry::getTableLocator()->get('listing', ['className' => $ucname]);
+			return TableRegistry::getTableLocator()->get(ucfirst($name), ['className' => $ucname]);
 		}
 
 		// use default generated (cake) table class
@@ -109,14 +114,14 @@ class DB {
 			# get last migration (if not given):
 			$last_migration_file = $last_migration_file ??
 			                       $migrations->find()->order(['migration_id' => 'DESC'])->first()->migration_file;
-		} catch (Exception $e) {
+		} catch(Exception $e) {
 			try {
 				// commit initial migration
 				$this->_migrate('0.sql');
 
 				// check if migration succeeded
 				$last_migration_file = $migrations->find()->order(['migration_id' => 'DESC'])->first()->migration_file;
-			} catch (Exception $e) {
+			} catch(Exception $e) {
 				// initial migration failed, give up now
 				http_response_code(500);
 				echo 'the database could not be set-up. Please check your initial migration';
@@ -170,7 +175,7 @@ function safe_save($object, $table) {
 
 	try {
 		return $table->save($object);
-	} catch (PDOException $e) {
+	} catch(PDOException $e) {
 		$_SESSION['feedback'] = [
 			'message' => 'Something went wrong.',
 			'errors'  => $e->getMessage(),
@@ -179,4 +184,41 @@ function safe_save($object, $table) {
 		return false;
 	}
 
+}
+
+/**
+ * @param int $room_id
+ * @param array $post
+ * @param Table $table
+ *
+ * @return bool|Entity
+ */
+function handle_add_listing($room_id, $post, $table) {
+
+	if (@$post['disable_listing'] == 'on') {
+		// don't do anything with listing
+	} else {
+		// check listing
+		$listing_data = [
+			'status'         => 'open',
+			'room_id'        => $room_id,
+			'available_from' => @$post['available_from']
+		];
+		if (!@$post['is_indefinite'] == 'on') {
+			// do something with available_to
+			$listing_data['available_to'] = @$post['available_to'];
+		}
+
+		$errors = validate_listing($listing_data, $table);
+		if ($errors) {
+			$_SESSION['feedback'] = ['message' => 'The room could not be listed.', 'errors' => $errors];
+			return false;
+		}
+		$new_listing = $table->newEntity($listing_data);
+
+		return safe_save($new_listing, $table);
+
+	}
+
+	return false;
 }
